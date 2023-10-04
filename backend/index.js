8f63b0c1a8c5d1abc1e0d789bdeeaf4965579048
Oshayer-Siddique <65402474+Oshayer-Siddique@users.noreply.github.com>
2023-10-04 22:30:19 +0600

    second

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,21 @@
 // backend/index.js
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const cors = require('cors');
 
+const cors = require('cors');
+const mongoose = require("mongoose");
+
+// mongoose
+//   .connect("mongodb://127.0.0.1:27017/Node11", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("Connected to MongoDB");
+//   })
+//   .catch((error) => {
+//     console.error("Error connecting to MongoDB:", error);
+//   });
 
 const app = express();
 const port = 5000;
@@ -13,15 +25,62 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors({ origin: 'http://localhost:3000' }));
 
+
+
+////////////////////////
+
+const peopleSchema = new mongoose.Schema({
+  name: {
+      type : String,
+      unique:true,
+
+  },
+  email : {
+      type : String,
+  },
+  message : {
+      type : String,
+  }
+})
+
+
+const People = mongoose.model("People",peopleSchema);
+
+//module.exports = People;
+
+
+///////////////////////////////////
+
+
 app.get("/" , (req,res)=>{
   res.send("DATA OK");
 })
 
+let peoplearray = [];
 
-app.post("/api/save", (req, res) => {
+
+app.post("/api/save", async (req, res) => {
   const { name, email, message } = req.body;
 
-  console.log(req.body);
+  const new_P = new People({
+    name,
+    email,
+    message,
+  });
+
+  //await new_P.save();
+
+  // /res.send("Data saved");
+  //res.send("OK");
+
+  peoplearray.push(req.body.name,req.body.message);
+
+
+  
+
+  res.send("Ok");
+
+  console.log(peoplearray);
 
 
 });
